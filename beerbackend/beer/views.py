29580c490e0af6dbac573925a0aa435126c07a27
Ethan Seither <ethan.seither@gmail.com>
2016-10-24 16:23:54 -0700

    Set up web functionality for checking statistics about a beer

--- beerbackend/beer/views.py
+++ beerbackend/beer/views.py
@@ -7,9 +7,34 @@ from beerbackend.beer.models import Beer
 from flask_restful import Resource, Api, reqparse, fields, marshal_with
 
 
+def get_color(color):
+    if color < 2.5:
+        return '#ffff45'
+    elif color < 3.5:
+        return '#ffe93e'
+    elif color < 5.5:
+        return '#fed849'
+    elif color < 8.5:
+        return '#ffa846'
+    elif color < 11.5:
+        return '#f49f44'
+    elif color < 14.5:
+        return '#d77f59'
+    elif color < 17.5:
+        return '#94523a'
+    elif color < 19.5:
+        return '#804541'
+    elif color < 23.5:
+        return '#5b342f'
+    elif color < 28.5:
+        return '#4c3b2b'
+    elif color < 35:
+        return '#38302e'
+    else:
+        return '#31302c'
+
+
 blueprint = Blueprint('beer', __name__, url_prefix='/beers', static_folder='../static')
-
-
 
 
 @blueprint.route('/add', methods=['GET', 'POST'])
@@ -31,7 +56,13 @@ def add():
             flash_errors(form)
     return render_template('beers/addbeer.html', form=form)
 
+
 @blueprint.route('/', methods=['GET'])
 def all():
     beers = Beer.query.all()
-    return render_template('beers/all.html', beers=beers)
+    return render_template('beers/all.html', beers=beers, str=str)
+
+@blueprint.route('/<int:beer_id>', methods=['GET'])
+def getbeer(beer_id):
+    beer = Beer.query.filter(Beer.id == beer_id).first()
+    return render_template('beers/onebeer.html', beer=beer, get_color=get_color)
